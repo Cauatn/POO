9c71cd0bdb8ddc75642fb0cc600cce8b1457a216
Cauã Tavares <39890456+CauanGx@users.noreply.github.com>
2023-09-27 17:00:03 -0300

    1.4

--- terraria/src/Npcs/Pre-Harmode/GoblinTrader.java
+++ terraria/src/Npcs/Pre-Harmode/GoblinTrader.java
@@ -5,7 +5,7 @@ public class GoblinTrader extends Npc {
     Inventario Inventario;
     
     public static void main(String [] args){
-        System.out.println("Oii");
+        System.out.println("");
     }
 
 }
