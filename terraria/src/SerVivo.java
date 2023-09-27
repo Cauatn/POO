00000485d65467edca7b70f0afb1049d717def98
David Rios <david__rios@hotmail.com>
2023-09-27 17:29:57 -0300

    Create SerVivo.java
    Criação da classe ser-vivo

---
+++ terraria/src/SerVivo.java
@@ -0,0 +1,40 @@
+package src;
+
+abstract class SerVivo {
+    int vida;
+    int dano;
+    int defesa;
+    int level;
+    int id;
+    
+    public int getVida() {
+        return vida;
+    }
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    public int getDano() {
+        return dano;
+    }
+    public void setDano(int dano) {
+        this.dano = dano;
+    }
+    public int getDefesa() {
+        return defesa;
+    }
+    public void setDefesa(int defesa) {
+        this.defesa = defesa;
+    }
+    public int getLevel() {
+        return level;
+    }
+    public void setLevel(int level) {
+        this.level = level;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+}
