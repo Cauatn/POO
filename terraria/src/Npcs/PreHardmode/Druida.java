2e8a205297513f104bd25e17e1d5490878054169
D-TeixeiraBraga <danielbraga2002@hotmail.com>
2023-09-29 15:57:33 -0300

    modifiquei os nome, parece ter funcionado!

--- terraria/src/Npcs/PreHardmode/Druida.java
+++ terraria/src/Npcs/PreHardmode/Druida.java
@@ -2,7 +2,7 @@ package src.Npcs.PreHardmode;
 
 import src.*;
 
-public class Druida extends Npc {
+public class Druida extends Npc{
     
     public static void main(String [] args){
         System.out.println("Oii");
