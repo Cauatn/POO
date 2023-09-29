0b130d953a077ecdfbf20db087e3c4fd8aeb2b79
D-TeixeiraBraga <danielbraga2002@hotmail.com>
2023-09-29 15:05:14 -0300

    Adicionei o começo dos acessórios

---
+++ terraria/src/Items/Acessorios/Movimento/Anklet_otw.java
@@ -0,0 +1,11 @@
+package src.Items.Acessorios.Movimento;
+
+import src.Items.Acessorios.AccAtt;
+
+public class Anklet_otw extends AccAtt{
+    
+    public Anklet_otw(){
+        MovSpeed += 0.1;
+    }
+
+}
