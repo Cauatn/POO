646db1105104754c5d0ea1e4fc3ee19f168477c8
David Rios <david__rios@hotmail.com>
2023-10-18 16:47:57 -0300

    Update Npc.java

--- terraria/src/Npcs/Npc.java
+++ terraria/src/Npcs/Npc.java
@@ -9,19 +9,30 @@ public abstract class Npc extends SerVivo{
     private String Ambiente;
     private String Gosta;
     private String Odeia;
-
+    
     Inventario Inventario;
+    
+    public String getAmbiente() {
+        return Ambiente;
+    }
+
+    public String getGosta() {
+        return Gosta;
+    }
+    public String getOdeia() {
+        return Odeia;
+     }
     
     public int getKB_Resist() {
         return KB_Resist;
     }
 
     public void setKB_Resist(int kB_Resist) {
-        KB_Resist = kB_Resist;
+        this.KB_Resist = kB_Resist;
     }
     
     public void setAmbiente(String ambiente) {
-        Ambiente = ambiente;
+        this.Ambiente = ambiente;
     }
 
     public void setOdeia(String odeia) {
