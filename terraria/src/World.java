e31ed13702c73a594580b60e53733383e08a9ae6
José Victor <josevictorcruzrb@gmail.com>
2023-10-23 17:21:00 -0300

    Update World.java
    transformando setters de world em static

--- terraria/src/World.java
+++ terraria/src/World.java
@@ -11,37 +11,37 @@ public abstract class World {
     public String getNome() {
         return Nome;
     }
-    public void setNome(String nome) {
+    public static void setNome(String nome) {
         Nome = nome;
     }
     public int getSeed() {
         return Seed;
     }
-    public void setSeed(int seed) {
+    public static void setSeed(int seed) {
         Seed = seed;
     }
     public String getTamanho() {
         return Tamanho;
     }
-    public void setTamanho(String tamanho) {
+    public static void setTamanho(String tamanho) {
         Tamanho = tamanho;
     }
     public String getModo() {
         return Modo;
     }
-    public void setModo(String modo) {
+    public static void setModo(String modo) {
         Modo = modo;
     }
     public String getDisease() {
         return Disease;
     }
-    public void setDisease(String disease) {
+    public static void setDisease(String disease) {
         Disease = disease;
     }
     public String getData_Criação() {
         return Data_Criação;
     }
-    public void setData_Criação(String data_Criação) {
+    public static void setData_Criação(String data_Criação) {
         Data_Criação = data_Criação;
     }
 }
